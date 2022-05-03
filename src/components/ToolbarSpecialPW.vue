84a7cb7567c8028cfefeb3978b53f2e7b208ba2f
apereg21 <apereg21@estudiantes.unileon.es>
2022-05-03 14:13:13 +0200

    0.3.3 DeleteUser implementado

--- src/components/ToolbarSpecialPW.vue
+++ src/components/ToolbarSpecialPW.vue
@@ -1,20 +1,28 @@
 <template>
-<v-card
-    color="white"
-    height="60px"
-  >
-  <v-toolbar color="#5B943D">
-    <v-btn class="ma-2" color="#F7DB5E" justify="end" v-on:click="goToMyProfile()">
-      <v-icon> mdi-arrow-left </v-icon>
-      Go Back
-    </v-btn>
-    <v-spacer/>
-    <v-btn class="ma-2" color="#F7DB5E" align="right" justify="end" v-on:click="logout()">
-      <v-icon> mdi-account-off </v-icon>
-      Delete Account
-    </v-btn>
-  </v-toolbar>
-</v-card>
+  <v-card color="white" height="60px">
+    <v-toolbar color="#5B943D">
+      <v-btn
+        class="ma-2"
+        color="#F7DB5E"
+        justify="end"
+        v-on:click="goToMyProfile()"
+      >
+        <v-icon> mdi-arrow-left </v-icon>
+        Go Back
+      </v-btn>
+      <v-spacer />
+      <v-btn
+        class="ma-2"
+        color="#F7DB5E"
+        align="right"
+        justify="end"
+        v-on:click="deleteAccount()"
+      >
+        <v-icon> mdi-account-off </v-icon>
+        Delete Account
+      </v-btn>
+    </v-toolbar>
+  </v-card>
 </template>
 
 <script>
@@ -26,25 +34,41 @@ export default {
       this.$router.push({
         name: "MyProfile",
       });
-      
     },
-    logout: function () {
-      this.$store.commit("SET_IDUSER", "")
-      this.$store.commit("SET_ROLE", "")
-      const headers = {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      };
-      axios
-        .get("http://localhost:3000/deleteUser/:" + this.$store.state.idUser, { headers })
-        .then((response) => {
-          console.log("Server response: " + response.data);
-          alert("Goodbye Mr/Mrs. \n"+response.data)
-        })
-        .catch((error) => {
-          console.log(error);
-          alert(error);
-        });      
+    goToHome: function () {
+      this.$router.push({
+        name: "Home",
+      });
+    },
+    deleteAccount: function () {
+      var itsok = confirm("Are you sure of that?");
+      if (itsok) {
+        const headers = {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        };
+        const idToNumber = parseInt(this.$store.state.idUser);
+        const postData = {
+          id: idToNumber,
+        };
+        axios
+          .post("http://localhost:3000/deleteUser", postData, { headers })
+          .then((response) => {
+            if (!response.data.includes("can't")) {
+              this.$store.commit("SET_IDUSER", "");
+              this.$store.commit("SET_ROLE", "");
+              console.log("Server response: " + response.data);
+              alert("Goodbye Mr/Mrs " + response.data);
+              this.goToHome();
+            } else {
+              alert(response.data);
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+            alert(error);
+          });
+      }
     },
   },
 };
